4bc9c3ed2c3fca3149a7cd16d894fd8c16c22fd8
Kamil Stangret <kamilstangret@wp.pl>
2017-12-27 19:42:29 +0100

    Add scripts.js with content

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
+		console.log('Nieprawidłowe dane')
+	}
+	else {
+		return (a*h/2)
+	}
+}
+console.log(getTriangleArea(10, 6))
+
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(1, 5);
+var triangle3Area = getTriangleArea(11, 2);
