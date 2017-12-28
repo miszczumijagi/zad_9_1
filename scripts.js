8af9965bd9c9c599dee85f75b2461123f238e463
Kamil Stangret <kamilstangret@wp.pl>
2017-12-28 20:27:04 +0100

    Change scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 function getTriangleArea(a, h) {
 	if (a <= 0 || h <= 0) {
-		console.log('Nieprawidłowe dane')
-	}
-	else {
-		return (a*h/2)
-	}
+		console.log('Nieprawidłowe dane');
+	} return (a*h/2);	
 }
 console.log(getTriangleArea(10, 6))
 
